de17ab829e6413cbde4790d57c43aa5623858ba5
JReuvers <reuve026@morris.umn.edu>
2020-10-07 21:33:30 -0500

    Create summarize_tree_ftw.c, haven't tested yet

---
+++ summarize_tree/summarize_tree_ftw.c
@@ -0,0 +1,25 @@
+#include <ftw.h>
+#include <stdio.h>
+
+#define MAX_FTW_DEPTH 16
+static int dirCount;
+static int files;
+
+static int callback(const char *fpath, const struct stat *sb, int typeflag) {
+	if(typeflag == FTW_F) {
+		files++;
+	} else if(typeflag == FTW_D) {
+		dirCount++;
+	}
+	return 0;
+}
+
+int main(int argc, char** argv) {
+
+	ftw(argv[1], callback, MAX_FTW_DEPTH);
+
+	printf("There were %i directories\n", dirCount);
+	printf("There were %i files", files);
+
+	return 0;
+}
