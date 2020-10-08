e4c992d1faa00e22277b9ca83232e2f700637b09
JReuvers <reuve026@morris.umn.edu>
2020-10-07 21:32:51 -0500

    Modify summarize_tree.c, haven't tested yet

--- summarize_tree/summarize_tree.c
+++ summarize_tree/summarize_tree.c
@@ -9,39 +9,41 @@
 static int num_dirs, num_regular;
 
 bool is_dir(const char* path) {
-  /*
-   * Use the stat() function (try "man 2 stat") to determine if the file
-   * referenced by path is a directory or not.  Call stat, and then use
-   * S_ISDIR to see if the file is a directory. Make sure you check the
-   * return value from stat in case there is a problem, e.g., maybe the
-   * the file doesn't actually exist.
-   */
+  struct stat buf;
+
+  if(stat(path, &buf) == 0) {
+	  if(S_ISDIR(stats.st_mode) > 0) {
+		  return true;
+	  } else {
+		  return false;
+	  }
+  } else {
+	  return false;
+  }
 }
 
-/* 
- * I needed this because the multiple recursion means there's no way to
- * order them so that the definitions all precede the cause.
- */
 void process_path(const char*);
 
 void process_directory(const char* path) {
-  /*
-   * Update the number of directories seen, use opendir() to open the
-   * directory, and then use readdir() to loop through the entries
-   * and process them. You have to be careful not to process the
-   * "." and ".." directory entries, or you'll end up spinning in
-   * (infinite) loops. Also make sure you closedir() when you're done.
-   *
-   * You'll also want to use chdir() to move into this new directory,
-   * with a matching call to chdir() to move back out of it when you're
-   * done.
-   */
+  num_dirs++;
+  DIR *newDir;
+  struct dirent *dirEntry;
+  
+  chdir(path);
+
+  if ((dirEntry = readdir(newDir)) != NULL) {
+	  if (strcmp(dirEntry->d_name, ".") != 0 && strcmp(dirEntry->d_name, "..") != 0) {
+		  num_dirs ++;
+		  process_path(dirEntry->d_name);
+	  }
+  }
+  
+  chdir("..");
+  closedir(newDir);
 }
 
 void process_file(const char* path) {
-  /*
-   * Update the number of regular files.
-   */
+	num_regular++;
 }
 
 void process_path(const char* path) {
